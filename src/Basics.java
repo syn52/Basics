e23813b224b37ebc489f5849ef8dce0cf5fdb686
Boris the Sheep <alenasynkova@Syn.local>
2017-09-23 05:53:26 -0700

    Initial commit

---
+++ src/Basics.java
@@ -0,0 +1,21 @@
+/**
+ * Execises to practice the basic syntax
+ */
+public class Basics {
+
+    public static void main(String[] args) {
+        // TODO: Write a Java program to print the sum of two numbers.
+
+
+
+        // TODO: Write a Java program to divide two numbers and print on the screen. Use both integers and doubles.
+
+
+
+        // TODO: Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers.
+
+
+
+        // TODO: Write a Java program to swap two variables. Print the before and after switching.
+    }
+}
